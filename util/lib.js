5aaceb637dcd2fb8b79aea9751bec6e7c9312f7e
dandan <liuyongshun2@126.com>
2019-09-15 22:19:58 +0800

    ss

--- util/lib.js
+++ util/lib.js
@@ -1,10 +1,22 @@
+const util = require('./index.js');
+
 class CommonMethods {
   filterAttr (obj, exc = ['_', '$0']) {
-    let temp = {}
+    let temp = {};
     Object.keys(obj).forEach(n => {
-      if (!exc.includes(n)) temp[n] = obj[n]
+      if (!exc.includes(n)) temp[n] = obj[n];
     })
-    return temp
+    if (temp.ignore) temp.i = temp.ignore;
+    if (temp.file) temp.f = temp.file;
+    return temp;
+  }
+  ignoreFile (dir, ignore, isFile) {
+    console.log(dir)
+    let subDir = `${dir}/${file}`;
+    dir.forEach(n => {
+      console.log(util.isFile(n), '3333')
+    })
+    return dir
   }
 }
 
--- util/tree.js
+++ util/tree.js
@@ -1,17 +1,21 @@
 const util = require('./index.js');
+const lib = require('./lib.js');
 
 let output = '';
 let init = '';
 
-function getTree (dir) {
+function getTree (dir, options) {
   const record = [];
-  if (util.isFile(dir)) return;
+  if (util.isFile(dir)) {
+    return;
+  }
   // get sub dir
   const files = util.getDir(dir);
-  // process.exit()
   // iteration sub dir and deal with data to get we want
   files.map((file, index) => {
     let subDir = `${dir}/${file}`;
+    if (options.i && subDir.includes(options.i) && util.isFile(subDir) === Boolean(options.f)) return
+    console.log(options, 'kkkklllll')
     const item = {
       name: file,
       dir: subDir,
@@ -26,7 +30,7 @@ function getTree (dir) {
     dirDeep = dirDeep.reduce(acc => ((dirDeep.length > 1 ? '│  ' : '') + acc), index === files.length - 1 ? '└─ ' : '├─ ');
     output += `${dirDeep}${dirname}\n`;
     if (!util.isFile(subDir)) {
-      item.children = getTree(subDir);
+      item.children = getTree(subDir, options);
     }
     record.push(item);
   });
